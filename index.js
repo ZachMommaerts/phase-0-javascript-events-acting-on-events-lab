ec0f24d70728988d42c767ffd1e8584bbd070d6b
Zach <zmommaerts@gmail.com>
2021-11-05 10:33:06 -0500

    completed assignment v2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+}
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
